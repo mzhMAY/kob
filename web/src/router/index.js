49112ba7ebaf89d3fb8a423849c815443088041c
mzhMAY <mazhonghuiMAY@163.com>
2023-10-27 18:17:25 +0800

    实现了导航栏 + 对战页面的地图和障碍物

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,6 +1,45 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import PkIndexView from '../views/pk/PkIndexView'
+import RecordIndexView from '../views/record/RecordIndexView'
+import RanklistIndexView from '../views/ranklist/RanklistIndexView'
+import NotFound from '../views/error/NotFound'
+import UserBotIndexView from '../views/user/bot/UserBotIndexView'
 
 const routes = [
+  {
+    path: "/",
+    name: "home",
+    redirect: "/pk/",
+  },
+  {
+    path: "/pk/",
+    name: "pk_index",
+    component: PkIndexView,
+  },
+  {
+    path: "/record/",
+    name: "record_index",
+    component: RecordIndexView,
+  },
+  {
+    path: "/ranklist/",
+    name: "ranklist_index",
+    component: RanklistIndexView,
+  },
+  {
+    path: "/404/",
+    name: "404",
+    component: NotFound,
+  },
+  {
+    path: "/user/bot/",
+    name: "user_bot_index",
+    component: UserBotIndexView,
+  },
+  {
+    path: "/:catchAll(.*)",
+    redirect: "/404/"
+  },
 ]
 
 const router = createRouter({
